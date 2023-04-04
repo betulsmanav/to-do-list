a11f3cd9a6553b32df544261e87daee89027d2b5
betul <betulsmanav@gmail.com>
2023-04-04 16:00:02 +0300

    new todo

--- app.js
+++ app.js
@@ -1,56 +1,84 @@
-const inputText = document.querySelector(".input");
-const addButton = document.querySelector(".add-btn");
-const form = document.querySelector(".form");
-const ul = document.querySelector("ul");
-// console.log(inputText);
+const input = document.querySelector("#todo-input");
+const addButton = document.querySelector("#todo-button");
+const todoList = document.querySelector("#todo-list");
+// console.log(object)
+let todos = JSON.parse(localStorage.getItem("todos")) || [];
+renderSavedTodos();
 
-const createTask = (task) => {
-    return `
-    <div class="list">
-            <li class="task">
-              ${task}
-            </li>
-            <button class="delete-btn">X</button>
-          </div>
-          `
+function renderSavedTodos() {
+  todos.forEach((todo) => {
+    createTodo(todo)
+  })
 }
 
 
-const counter = () => {
-    let taskCount = ul.getElementsByClassName("list").length;
-    let spanCount = document.querySelector("#task-count");
-    spanCount.innerHTML = taskCount;
-    console.log(spanCount.innerHTML);
 
+window.onload = function () {
+  input.focus();
+};
+
+function createTodo(todo) {
+  const { id, isDone, content } = todo;
+ 
+  todoList.innerHTML += `
+  <li id=${id} class='${isDone ? 'checked' : ''} '>
+
+  <i class="check fa-sharp fa-solid fa-check"></i>
+  <p>${content}</p>
+  <i class="delete fa-sharp fa-solid fa-trash"></i>
+  </li>`;
 }
-window.addEventListener("load", () => {
-    counter();
+
+addButton.addEventListener("click", () => {
+  // console.log(todoList.innerHTML)
+  if (!input.value) {
+    alert("Please enter a to-do")
+  } else {
+    const todoObject = {
+      id: new Date().getTime(),
+      isDone: false,
+      content:input.value,
+    }
+    todos.push(todoObject)
+    localStorage.setItem('todos',JSON.stringify(todos))
+    createTodo(todoObject)
+    input.value = "";
     
+  }
+});
+
+todoList.addEventListener('click', (e) => {
+  // console.log(e.target.parentElement)
+  const id = e.target.parentElement.getAttribute('id');
+
+  if (e.target.classList.contains('delete')) {
+    todos = todos.filter((todo) => todo.id != id);
+    localStorage.setItem('todos', JSON.stringify(todos));
+    e.target.parentElement.remove()
+  }
+
+  if (e.target.classList.contains('check')) {
+    todos.map((todo, index) => {
+      if (todo.id == id) {
+        todos[index].isDone = !todos[index].isDone;
+      }
+    });
+    localStorage.setItem('todos',JSON.stringify(todos))
+    if (e.target.parentElement.classList.contains('checked')) {
+      e.target.parentElement.classList.remove('checked')
+    } else {
+      e.target.parentElement.classList.add('checked')
+        
+    }
+  }
+ 
+ 
 })
 
-addButton.addEventListener("click", e => {
-    e.preventDefault();
-    if (inputText.value !="") {
-        ul.innerHTML += createTask(inputText.value);
-        form.reset();
-        counter();
-
-    } else {
-        alert("bos task eklenemez.")
-    };
-        
-
-});
-
-ul.addEventListener("click", e => {
-    if (e.target.classList.contains("delete-btn")) {
-        e.target.parentElement.remove();
-    counter();
-
-    }
-    if (e.target.classList.contains("task")) {
-        e.target.parentElement.style.background = "lightgray";
-    }
-       
-});
+input.addEventListener('keydown', (e) => {
+  // console.log(e.target)
+    if (e.code == 'Enter') {
+      addButton.click()
+  }
+})
 
